0c223f05f3901f433bfee2c6ee8585af21e95a2a
Jacco Houtekamer <jacco.houtekamer@ordina.nl>
2019-06-05 21:46:53 +0200

    Code prepped for discount

--- src/test/java/nl/bank/levensverzekeringservice/LevensverzekeringControllerTest.java
+++ src/test/java/nl/bank/levensverzekeringservice/LevensverzekeringControllerTest.java
@@ -3,6 +3,7 @@ package nl.bank.levensverzekeringservice;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
